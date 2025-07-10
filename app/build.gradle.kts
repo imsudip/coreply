858e6db232f87c478bdba561dd3d3d1beeb9b82a
frameartist <seanxionghk@gmail.com>
2025-07-11 01:36:24 +0800

    remove retrieve interactive window flag (causing problems on samsung), fix trailing suggestions not showing completely on <API33 devices bug, use proper display cutout display mode for better compatability

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "app.coreply.coreplyapp"
         minSdk = 30
         targetSdk = 35
-        versionCode = 7
-        versionName = "2.1.0"
+        versionCode = 8
+        versionName = "2.1.1"
         vectorDrawables.useSupportLibrary = true
     }
     compileOptions {
