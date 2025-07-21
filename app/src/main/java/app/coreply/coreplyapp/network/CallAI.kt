303adf2e2df5a4753f708472df7feb788646a2d3
frameartist <seanxionghk@gmail.com>
2025-07-21 15:44:34 +0800

    Remove occasional 'Message I sent' text from responses

--- app/src/main/java/app/coreply/coreplyapp/network/CallAI.kt
+++ app/src/main/java/app/coreply/coreplyapp/network/CallAI.kt
@@ -21,37 +21,32 @@ package app.coreply.coreplyapp.network
 
 import android.content.Context
 import android.util.Log
+import app.coreply.coreplyapp.data.PreferencesManager
+import app.coreply.coreplyapp.utils.ChatContents
+import app.coreply.coreplyapp.utils.SuggestionUpdateListener
 import com.aallam.openai.api.chat.ChatCompletionRequest
 import com.aallam.openai.api.chat.ChatMessage
 import com.aallam.openai.api.chat.ChatRole
+import com.aallam.openai.api.core.RequestOptions
 import com.aallam.openai.api.model.ModelId
 import com.aallam.openai.client.OpenAI
 import com.aallam.openai.client.OpenAIConfig
 import com.aallam.openai.client.OpenAIHost
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.conflate
-import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.launch
-import app.coreply.coreplyapp.utils.ChatContents
-import app.coreply.coreplyapp.data.PreferencesManager
-import app.coreply.coreplyapp.utils.SuggestionUpdateListener
-import com.aallam.openai.api.core.RequestOptions
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.flow.sample
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.launch
 import java.util.concurrent.ConcurrentHashMap
-import kotlin.compareTo
-import kotlin.text.get
 
 
 class SuggestionStorageClass(private var listener: SuggestionUpdateListener? = null) {
     private val _suggestionHistory = ConcurrentHashMap<String, String>()
     private val PUNCTUATIONS = listOf(
         "!", "\"", ")", ",", ".", ":",
-        ";", "?", "]", "~", "，","。", "：", "；", "？", "）","】", "！", "、", "」",
+        ";", "?", "]", "~", "，", "。", "：", "；", "？", "）", "】", "！", "、", "」",
     )
     private val PUNCTUATIONS_REGEX = "(?=[!\")\\],.:;?~，。：；？）】！、」])".toRegex()
 
@@ -61,7 +56,7 @@ class SuggestionStorageClass(private var listener: SuggestionUpdateListener? = n
         if (parts.size < 2) return parts
 
         // Check if the last part is just punctuation
-        else{
+        else {
             val lastPart = parts.last()
             if (lastPart.length == 1 && PUNCTUATIONS.contains(lastPart)) {
                 // Merge the last punctuation with the second-to-last part
@@ -88,9 +83,21 @@ class SuggestionStorageClass(private var listener: SuggestionUpdateListener? = n
         }
         return key
     }
+
     fun String.replaceWhiteSpaces(): String {
         return this.replace("\\s+".toRegex(), " ")
     }
+    fun String?.removeMessageISent(): String {
+        Log.v("CallAI", "Response: $this")
+        if (this == null) return ""
+        else if (this.startsWith("Message I sent: ")) {
+            return this.substring("Message I sent: ".length)
+        } else if (this.startsWith("Message I received: ")) {
+            return this.substring("Message I received: ".length)
+        }
+        return this
+    }
+
     fun getSuggestion(toBeCompleted: String): String? {
         if (toBeCompleted.isBlank()) {
             if (_suggestionHistory.containsKey("")) {
@@ -103,7 +110,8 @@ class SuggestionStorageClass(private var listener: SuggestionUpdateListener? = n
                 val starting = toBeCompleted.substring(i)
                 val suggestion = _suggestionHistory[target]!!
                 if (starting.isEmpty() || (suggestion.startsWith(starting) &&
-                            suggestion.length > starting.length)) {
+                            suggestion.length > starting.length)
+                ) {
                     return suggestion.substring(starting.length)
                 }
             }
@@ -126,16 +134,32 @@ class SuggestionStorageClass(private var listener: SuggestionUpdateListener? = n
     }
 
     fun updateSuggestion(typingInfo: TypingInfo, newSuggestion: String) {
-        if(newSuggestion.replaceWhiteSpaces().lowercase().startsWith(typingInfo.currentTyping.replaceWhiteSpaces().lowercase())) {
-            val frontTrimmedSuggestion = newSuggestion.replaceWhiteSpaces().substring(typingInfo.currentTyping.replaceWhiteSpaces().length)
+        if (newSuggestion.replaceWhiteSpaces().removeMessageISent().lowercase()
+                .startsWith(typingInfo.currentTyping.replaceWhiteSpaces().removeMessageISent().lowercase())
+        ) {
+            val frontTrimmedSuggestion = newSuggestion.replaceWhiteSpaces().removeMessageISent()
+                .substring(typingInfo.currentTyping.replaceWhiteSpaces().removeMessageISent().length)
             val splittedText = splitAndKeepPunctuations(frontTrimmedSuggestion)
 //            Log.v("CallAI", "Splitted text: $splittedText")
             for (i in 0..splittedText.size - 2) {
 //                Log.v("CallAI", getKeyFromText(typingInfo.currentTyping + splittedText.subList(0, i + 1).joinToString("")))
-                addSuggestionWithoutReplacement(getKeyFromText(typingInfo.currentTyping + splittedText.subList(0, i + 1).joinToString("")), splittedText[i + 1])
+                addSuggestionWithoutReplacement(
+                    getKeyFromText(
+                        typingInfo.currentTyping + splittedText.subList(
+                            0,
+                            i + 1
+                        ).joinToString("")
+                    ), splittedText[i + 1]
+                )
             }
-            addSuggestionWithoutReplacement(getKeyFromText(typingInfo.currentTyping), if( splittedText.isNotEmpty()) splittedText[0] else "")
-            listener?.onSuggestionUpdated(typingInfo, frontTrimmedSuggestion) // huh actually the arguments are unused
+            addSuggestionWithoutReplacement(
+                getKeyFromText(typingInfo.currentTyping),
+                if (splittedText.isNotEmpty()) splittedText[0] else ""
+            )
+            listener?.onSuggestionUpdated(
+                typingInfo,
+                frontTrimmedSuggestion
+            ) // huh actually the arguments are unused
         }
     }
 }
@@ -147,10 +171,14 @@ data class TypingInfo(val pastMessages: ChatContents, val currentTyping: String)
 }
 
 @OptIn(FlowPreview::class)
-open class CallAI(open val suggestionStorage: SuggestionStorageClass, private val context: Context?) {
+open class CallAI(
+    open val suggestionStorage: SuggestionStorageClass,
+    private val context: Context?
+) {
     private val coroutineScope = CoroutineScope(Dispatchers.IO)
     private val networkScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
-    private val preferencesManager = if (context==null) null else PreferencesManager.getInstance(context)
+    private val preferencesManager =
+        if (context == null) null else PreferencesManager.getInstance(context)
 
     // Flow to handle debouncing of user input
     private val userInputFlow = MutableSharedFlow<TypingInfo>(replay = 1)
@@ -162,7 +190,7 @@ open class CallAI(open val suggestionStorage: SuggestionStorageClass, private va
             userInputFlow // adjust debounce delay as needed
                 .debounce(200)
                 .collect { typingInfo ->
-                    networkScope.launch{
+                    networkScope.launch {
                         fetchSuggestions(typingInfo)
                     }
                 }
@@ -215,7 +243,12 @@ open class CallAI(open val suggestionStorage: SuggestionStorageClass, private va
                 typingInfo.pastMessages.getCoreply2Format() + "\nIn addition to the message I sent,\n" +
                 "What else should I send? Or start a new topic?"
         if (typingInfo.currentTyping.isNotBlank()) {
-            userPrompt += "The reply should start with '${typingInfo.currentTyping.replace("\\s+".toRegex(), " ")}'\n"
+            userPrompt += "The reply should start with '${
+                typingInfo.currentTyping.replace(
+                    "\\s+".toRegex(),
+                    " "
+                )
+            }'\n"
         }
         val request = ChatCompletionRequest(
             temperature = preferencesManager.temperatureState.value.toDouble(),
@@ -246,6 +279,7 @@ open class CallAI(open val suggestionStorage: SuggestionStorageClass, private va
                 )
             )
         )
+
         //Log.v("CallAI", "Response: ${response.choices.first().message.content?.trim()}")
         return response.choices.first().message.content?.trim() ?: ""
     }
